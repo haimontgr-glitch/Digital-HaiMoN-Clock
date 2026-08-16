c43d36a7d77a8adc184ce61826cfd1d20b997378
haimontgr-glitch <haimontgr@gmail.com>
2026-08-16 14:02:19 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function updateClock() {
-const now = new Date();
-const h = String(now.getHours()).padStart(2, '0');
-const m = String(now.getMinutes()).padStart(2, '0');
-const s = String(now.gerSeconds()).padStart(2, '0');
-document.getElementById('clock').textContent = h + ':' + m + ':' + s;
+  const now = new Date();
+  const h = String(now.getHours()).padStart(2, '0');
+  const m = String(now.getMinutes()).padStart(2, '0');
+  const s = String(now.getSeconds()).padStart(2, '0');
+  document.getElementById('clock').textContent = h + ':' + m + ':' + s;
 }
+
 setInterval(updateClock, 1000);
 updateClock();
